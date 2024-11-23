0a493d0b24fe3dbddec51d3c143fef1c23f9b07b
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2024-11-23 21:11:54 +0530

    Add files via upload

---
+++ src/pages/Page4.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Page4 = () => {
+  return (
+    <div className='h-screen p-3 bg-white'>
+      <div className='h-full w-full texture flex gap10 rounded-[30px]'>
+        <div className='relative p-10 text-[20vw] ml-6 leading-none flex flex-col text-gray-600 font-[anzo1]'>
+          <h1 className='mt-[10px]'>WHAT</h1>
+          <h1 className='text-white uppercase -mt-12 '>I DO</h1>
+        </div>
+        <div>
+          <img src="../../Asset/moonside.png" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page4
